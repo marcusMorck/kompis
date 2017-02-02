0fdc67a50c30eaa19aed6f0045890acda281e96f
Marcus Mörck <marcus.morck@gmail.com>
2017-02-02 09:25:49 +0100

    Add files via upload

---
+++ inloggning/loggin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+    <form action="loggincheck.php" method="POST">
+        E-mail:<br>
+        <input type="text" name="email" value="Email">
+        <br>
+        Last name:<br>
+        <input type="text" name="password" value="Password">
+        <br><br>
+        <input type="submit" value="logga in">
+    </form> 
+
+        
+    </body>
+</html>
