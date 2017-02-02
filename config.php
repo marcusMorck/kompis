519e7de2e3a495d4649e20b39a4c24f71fda5686
Fredrikhietala <hietala.fredrik@gmail.com>
2017-02-02 11:30:54 +0100

    config
    connection

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+	$host = '127.0.0.1'; // localhost, den här datorn
+	$db = 'kompis';
+	$user = 'root';
+	$password = 'root';
+	$charset = 'utf8';
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false  ];
+	$pdo = new PDO($dsn, $user, $password, $options);
+
+ ?>
