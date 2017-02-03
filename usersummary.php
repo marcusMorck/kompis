895a8589867a3a183d96c9441b5864c7a29bbb66
Fredrikhietala <hietala.fredrik@gmail.com>
2017-02-03 12:57:39 +0100

    Add files via upload
    Summering av vad man skrivit in när man registrerade sig

---
+++ usersummary.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Summering av användaruppgifter</title>
+</head>
+<body>
+
+<?php
+require "config.php";
+
+?>
+
+<div style="border:1px solid black;">
+
+<?php
+
+
+$x=$_SESSION['userobj']['name'];
+echo "<p>Namn: $x</p>";
+$x=$_SESSION['userobj']['epost'];
+echo "<p>E-post: $x</p>";
+$x=$_SESSION['userobj']['adress'];
+echo "<p>Adress: $x</p>";
+$x=$_SESSION['userobj']['zipCode'];
+echo "<p>Postnummer: $x</p>";
+$x=$_SESSION['userobj']['city'];
+echo "<p>Ort: $x</p>";
+$x=$_SESSION['userobj']['phoneNumber'];
+echo "<p>Telefonnummer: $x</p>";
+
+?>	
+
+</div>
+
+
+</body>
+</html>
