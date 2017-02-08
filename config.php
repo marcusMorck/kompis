d8e558eb859078712b1cd36b670577fb2d8d89ef
Marcus Mörck <marcus.morck@gmail.com>
2017-02-08 19:25:49 +0100

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,9 @@
 	$options = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 				PDO::ATTR_EMULATE_PREPARES   => false  ];
+	try { //Try to connect to database
 	$pdo = new PDO($dsn, $user, $password, $options);
-
+	} catch (PDOException $e){ //if not connected succesfully, give me an error message
+		echo $e->getMessage();
+	}
  ?>
