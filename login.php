81c2bad81b8a90fad582840f12c8952d118dd7df
Marcus Mörck <marcus.morck@gmail.com>
2017-02-11 22:13:35 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,52 +1,52 @@
 <?php
+session_start(); //needs to be included in all files
+require "../includes/config.php";
 
-session_start();
-require "config.php";
 
-if(!isset($_SESSION['role']){
-	
-	$email = $_POST['email'];
-	$password = $_POST['password'];
+if (!isset($_SESSION['session_id'])) //if the session already is set
+{
+    if (isset($_POST['submit'])) //checks if the user has clicked the submit button
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-	$sqlquery = "SELECT * FROM users WHERE email='$email' AND hashedPw='$password'";
+        $sqlquery = "SELECT * FROM users WHERE email='$email'";
 
-	$stm = $pdo->prepare($sqlquery);
-	$row = $stm->fetch();
-	$pwd = hash("sha512", $password . $row['salt'] );
-	$password = $pwd;
+        $statement = $pdo->prepare($sqlquery);
+        $statement->execute();
+        $rows = $statement->fetch(PDO::FETCH_ASSOC);
 
-	if ($stm->fetchColumn() > 0)
-	{
-		if ($row['role'] === ""){
-			//Inloggad som användare
-			$_SESSION['role'] = $row['role'];
-			header("Location: index.php");
-		}
-		else if ($_SESSION['role'] === "barnvakt"){
-			//Inloggad som barnvakt
-			$_SESSION['role'] = $row['role'];
-			header("Location: index.php");
-		}
-		else if ($_SESSION['role'] === "läxhjälp"){
-			//Inloggad som läxhjälp
-			$_SESSION['role'] = $row['role'];
-			header("Location: index.php");
-		}
-		else if ($_SESSION['role'] === "admin"){
-			//Inloggad som admin
-			$_SESSION['role'] = $row['role'];
-			header("Location: index.php");
-		}
-	}
-	else
-	{
-		echo "Fel användarnamn eller lösenord, var vänlig försök igen! <br>
-		klicka <a href='loggin.php'>Här</a> för att göra ett nytt försök!";
-	}	
-
+        if ($statement->rowCount() === 1) //check if we have a row with the user
+        { 
+            if (password_verify($password, $rows['hashedPw'])) // Check if the password that is typed in equals to the stored hash
+            {
+                // Correct password - create the session and set it equal to the user id in the database
+                $_SESSION['session_id'] = $rows['id'];
+                header('Location: ../html/index.html');
+            } 
+            else
+            {
+                // Wrong password - write out error message, redirect back to login page
+                echo "felaktig lösenord eller användarnamn, var vänlig försök igen!";
+                header('Refresh:5 url=../html/login.html');
+            }
+        }
+        else
+        {
+            // The user doesn't exist - write out error message, redirect back to login page
+            echo "felaktig lösenord eller användarnamn, var vänlig försök igen!";
+            header('Refresh:5 url=../html/login.html');
+        }
+    }
+    else
+    {
+        //The submit button hasn't been clicked - write out error message, redirect back to login page
+        echo "du har ingen behörighet till denna sidan, var vänlig försök igen!";
+        header('Refresh:5 url=../html/index.html');
+    }
 }
-else{
-		echo "Du är inte inloggad, var vänlig logga in!<br>
-		klicka <a href='login.php'>Här</a> för att göra ett nytt försök!";
+else
+{
+    header('Refresh:5 url=../html/index.html');
+    echo "Du är redan inloggad";  
 }
-?>
