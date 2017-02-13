49a25c7a4efc033977875bbb49e035f273816381
Fredrikhietala <hietala.fredrik@gmail.com>
2017-02-13 13:57:15 +0100

    Update readbooking.php

--- readbooking.php
+++ readbooking.php
@@ -11,13 +11,26 @@ require 'config.php';
 
 <?php
 
+		
+	if (isset($_POST['idbabysitter'])) {
+		$sqlDelete = "DELETE FROM `bookingbabysitter` WHERE `id` = :idbabysitter";
+		$stmDelete = $pdo->prepare($sqlDelete);
+		$stmDelete->execute(['idbabysitter' => $_POST['idbabysitter']]);
+		echo "<p>Du tog bort din bokning!</p>";
+	}elseif (isset($_POST['idtutor'])) {
+		$sqlDelete = "DELETE FROM `bookingtutor` WHERE `id` = :idtutor";
+		$stmDelete = $pdo->prepare($sqlDelete);
+		$stmDelete->execute(['idtutor' => $_POST['idtutor']]);
+		echo "<p>Du tog bort din bokning!</p>";
+	}
+
 	$sql = "SELECT * FROM `bookingtutor`";
 	$stm = $pdo->prepare($sql);
 	$stm->execute([]);
 
 		echo "<h2>Läxhjälpsbokningar</h2>
 			<table>
-			<tr><th>Name</th> <th>Ämne</th> <th>Tutor</th> <th>Starttid</th> <th>Sluttid</th>
+			<tr><th>Name</th> <th>Ämne</th> <th>Tutor</th> <th>Starttid</th> <th>Sluttid</th> 
 			</tr>";
 
 	foreach ($stm as $row) {
@@ -27,8 +40,16 @@ require 'config.php';
 		$startTime = $row['startTime'];
 		$endTime = $row['endTime'];
 			echo "<tr>
-				<td>$userName</td> <td>$subject</td> <td>$tutor</td> <td>$startTime</td> <td>$endTime</td>
-				</tr>";
+				<td>$userName</td> <td>$subject</td> <td>$tutor</td> <td>$startTime</td> <td>$endTime</td>";
+				?>
+				<td>
+				<form action="readbooking.php" method="post">
+				<input type="hidden" name="idtutor" value="<?php echo $row['id'];?>" />
+				<button type="submit">Ta bort</button>
+				</form>
+				</td>
+				<?php
+			echo "</tr>";
 		} 
 		echo "</table>";
 
@@ -43,6 +64,7 @@ require 'config.php';
 			</tr>";
 
 	foreach ($stm as $row) {
+		$id = $row['id'];
 		$userName = $row['userName'];
 		$antalBarn = $row['children'];
 		$babysitter = $row['babysitter'];
@@ -50,10 +72,19 @@ require 'config.php';
 		$endTime = $row['endTime'];
 
 		echo "<tr>
-			<td>$userName</td> <td>$antalBarn</td> <td>$babysitter</td> <td>$startTime</td> <td>$endTime</td>
-			</tr>";
+			<td>$userName</td> <td>$antalBarn</td> <td>$babysitter</td> <td>$startTime</td> <td>$endTime</td>";
+			?>
+			<td>
+			<form action="readbooking.php" method="post">
+			<input type="hidden" name="idbabysitter" value="<?php echo $row['id'];?>" />
+			<button type="submit">Ta bort</button>
+			</form>
+			</td>
+			<?php
+		echo "</tr>";
 	}
 		echo "</table>";
+
 
 ?>
 
