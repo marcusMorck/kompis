ecbaa18eaffcc49e54acf1982403936e0e6e7665
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-23 20:38:30 +0100

    Updated show babysitter & tutor

--- includes/show_tutor.php
+++ includes/show_tutor.php
@@ -3,8 +3,9 @@ require "config.php";
 
 $startTime = $_GET['starttime'];
 $endTime = $_GET['endtime'];
-
+// hämtar start- och sluttid från boka barnvakt
 $sql = "SELECT `name`, `startTime`, `endTime` FROM `users` LEFT JOIN `bookingtutor` ON `users`.`name` = `bookingtutor`.`tutor` WHERE `startTime` != '" . $startTime . "' AND `endTime` != '" . $endTime . "' GROUP BY `name`";
+// visar alla barnvakter som inte är upptagna vald tid
 $stm = $pdo->prepare($sql);
 $stm->execute([]);
 
