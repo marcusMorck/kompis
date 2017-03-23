c5e4f5e5493dcaf93cd852abbfcc72282363a8f5
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-23 20:28:42 +0100

    Updated book_tutor.php

--- includes/book_tutor.php
+++ includes/book_tutor.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Boka läxhjälp</title>
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-<?php
+
 require 'config.php';
 
 if (isset($_POST['book'])) {
@@ -26,57 +16,24 @@ if (isset($_POST['book'])) {
 	$endTime = $_POST['endtime'];
 
 	if (!isset($reg_error)) {
-		
-		$stm = $pdo->prepare("INSERT INTO `bookingtutor`(`userName`, `subject`, `tutor`, `startTime`, `endTime`) VALUES ('$userName', '$subject', '$tutor', '$startTime', '$endTime')");
+		// ifall det inte är några fel läggs bokningen in i databasen
+		$stm = $pdo->prepare("INSERT INTO `bookingtutor`(`userName`, `userId`, `subject`, `tutor`, `startTime`, `endTime`) VALUES ('$userName', :userId, '$subject', '$tutor', '$startTime', '$endTime')");
 
 		try {
-			$stm->execute();
+			$stm->execute(['userId' => $_SESSION['session_id']]);
 		}
 		catch (PDOException $e) {
 			echo "Error: " . $e->getMessage();
 		}
 
 		$_SESSION['userid'] = $pdo->lastInsertId();
-
+		// sparar info till en bookingsummary, som vi inte använder
 		$_SESSION['bookobj'] = $_POST;
 
 		echo "<script type='text/javascript'>
-	   	document.location.href = '../html/my_page.html';
+	   	document.location.href = '../html/my_page.php';
 		</script>";
 	    exit;
 	}
 }
 ?>
-
-<h1>Boka Läxhjälp</h1>
-
-
-	<form action='booktutor.php' method='post'>
-		<p>Namn:</p>
-		<input type='text' name='name' value=''>
-		<p>Ämne:</p>
-		<input type='text' name='subject' value=''> 
-		<p>Starttid:</p>
-		<input type='datetime-local' name='starttime'>
-		<p>Sluttid:</p>
-		<input type='datetime-local' name='endtime'>
-		<p>Läxhjälp:</p>
-		<select name ='tutor' id="tutor">
-			<option></option>
-			<?php
-				$stm = $pdo->prepare("SELECT `name` FROM `users` WHERE `role` = :roll");
-				$stm->execute(['roll' => 'Läxhjälp']);
-					foreach ($stm as $row) {
-						$name = $row['name'];
-						echo "<option>$name</option>";
-						}
-
-			?>
-		</select>
-		<br><br>
-		<button type='submit' name='book'>Boka</button>
-	</form>
-
-
-</body>
-</html>
