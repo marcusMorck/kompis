556ecbc45536e3b75ed941c4eafc2834372bab99
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-23 20:36:39 +0100

    Updated read_booking.php

--- includes/read_booking.php
+++ includes/read_booking.php
@@ -1,7 +1,12 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Mina bokningar</title>
+
 </head>
 <body>
 <?php
@@ -10,81 +15,96 @@ require 'config.php';
 <h1>Mina Bokningar</h1>
 
 <?php
-
-		
+// kollar om användaren är inloggad
+if (isset($_SESSION['session_id'])) {
+	
+		// tar bort bokningen när man trycker på knappen ta bort
 	if (isset($_POST['idbabysitter'])) {
 		$sqlDelete = "DELETE FROM `bookingbabysitter` WHERE `id` = :idbabysitter";
 		$stmDelete = $pdo->prepare($sqlDelete);
 		$stmDelete->execute(['idbabysitter' => $_POST['idbabysitter']]);
-		echo "<p>Du tog bort din bokning!</p>";
-	}elseif (isset($_POST['idtutor'])) {
+
+		echo "<script type='text/javascript'>
+	   	document.location.href = '../html/my_page.php';
+		</script>";
+	    exit;
+	}
+		// tar bort bokningen när man trycker på knappen ta bort
+	if (isset($_POST['idtutor'])) {
 		$sqlDelete = "DELETE FROM `bookingtutor` WHERE `id` = :idtutor";
 		$stmDelete = $pdo->prepare($sqlDelete);
 		$stmDelete->execute(['idtutor' => $_POST['idtutor']]);
-		echo "<p>Du tog bort din bokning!</p>";
+
+		echo "<script type='text/javascript'>
+	   	document.location.href = '../html/my_page.php';
+		</script>";
+	    exit;
 	}
+		$now = date("Y-m-d H:i:s"); // dagens datum och tid på servern
+		$sql = "SELECT * FROM `bookingtutor` WHERE `userId` = :userid";
+		$stm = $pdo->prepare($sql);
+		$stm->execute(['userid' => $_SESSION['session_id']]); 
+		// kollar att userid stämmer överens och visar bara den inloggades bokningar
+			echo "<h2>Läxhjälpsbokningar</h2>
+				<table>
+				<tr><th>Name</th> <th>Ämne</th> <th>Tutor</th> <th>Starttid</th> <th>Sluttid</th> 
+				</tr>";
+		foreach ($stm as $row) {
+			$userName = $row['userName'];
+			$subject = $row['subject'];
+			$tutor = $row['tutor'];
+			$startTime = $row['startTime'];
+			$endTime = $row['endTime'];
+				if ($startTime > $now) { 
+					// jämför starttid för bokning med dagens datum och visar bara kommande bokningar
+					echo "<tr>
+					<td>$userName</td> <td>$subject</td> <td>$tutor</td> <td>$startTime</td> <td>$endTime</td>";
+					?>
+					<td>
+					<form action="../includes/readbooking.php" method="post">
+					<input type="hidden" name="idtutor" value="<?php echo $row['id'];?>" />
+					<button type="submit">Ta bort</button>
+					</form>
+					</td>
+					<?php
+					echo "</tr>";
+				}
+				} 
+				echo "</table>";
 
-	$sql = "SELECT * FROM `bookingtutor`";
-	$stm = $pdo->prepare($sql);
-	$stm->execute([]);
-
-		echo "<h2>Läxhjälpsbokningar</h2>
-			<table>
-			<tr><th>Name</th> <th>Ämne</th> <th>Tutor</th> <th>Starttid</th> <th>Sluttid</th> 
-			</tr>";
-
-	foreach ($stm as $row) {
-		$userName = $row['userName'];
-		$subject = $row['subject'];
-		$tutor = $row['tutor'];
-		$startTime = $row['startTime'];
-		$endTime = $row['endTime'];
-			echo "<tr>
-				<td>$userName</td> <td>$subject</td> <td>$tutor</td> <td>$startTime</td> <td>$endTime</td>";
-				?>
-				<td>
-				<form action="readbooking.php" method="post">
-				<input type="hidden" name="idtutor" value="<?php echo $row['id'];?>" />
-				<button type="submit">Ta bort</button>
-				</form>
-				</td>
-				<?php
-			echo "</tr>";
-		} 
+		$sql = "SELECT * FROM `bookingbabysitter` WHERE `userId` = :userid";
+		$stm = $pdo->prepare($sql);
+		$stm->execute(['userid' => $_SESSION['session_id']]);
+		// kollar att userid stämmer överens och visar bara den inloggades bokningar
+			echo "<h2>Barnvaktsbokningar</h2>
+				<table>
+				<tr><th>Name</th> <th>Antal barn</th> <th>Barnvakt</th> <th>Starttid</th> <th>Sluttid</th>
+				</tr>";
+		foreach ($stm as $row) {
+			$id = $row['id'];
+			$userName = $row['userName'];
+			$antalBarn = $row['children'];
+			$babysitter = $row['babysitter'];
+			$startTime = $row['startTime'];
+			$endTime = $row['endTime'];
+				if ($startTime > $now) {
+					// jämför starttid för bokning med dagens datum och visar bara kommande bokningar
+					echo "<tr>
+					<td>$userName</td> <td>$antalBarn</td> <td>$babysitter</td> <td>$startTime</td> <td>$endTime</td>";
+					?>
+					<td>
+					<form action="../includes/readbooking.php" method="post">
+					<input type="hidden" name="idbabysitter" value="<?php echo $row['id'];?>" />
+					<button type="submit">Ta bort</button>
+					</form>
+					</td>
+					<?php
+					echo "</tr>";
+				}
+				}
 		echo "</table>";
 
-
-	$sql = "SELECT * FROM `bookingbabysitter`";
-	$stm = $pdo->prepare($sql);
-	$stm->execute([]);
-
-		echo "<h2>Barnvaktsbokningar</h2>
-			<table>
-			<tr><th>Name</th> <th>Antal barn</th> <th>Barnvakt</th> <th>Starttid</th> <th>Sluttid</th>
-			</tr>";
-
-	foreach ($stm as $row) {
-		$id = $row['id'];
-		$userName = $row['userName'];
-		$antalBarn = $row['children'];
-		$babysitter = $row['babysitter'];
-		$startTime = $row['startTime'];
-		$endTime = $row['endTime'];
-
-		echo "<tr>
-			<td>$userName</td> <td>$antalBarn</td> <td>$babysitter</td> <td>$startTime</td> <td>$endTime</td>";
-			?>
-			<td>
-			<form action="readbooking.php" method="post">
-			<input type="hidden" name="idbabysitter" value="<?php echo $row['id'];?>" />
-			<button type="submit">Ta bort</button>
-			</form>
-			</td>
-			<?php
-		echo "</tr>";
-	}
-		echo "</table>";
-
+}
 
 ?>
 
